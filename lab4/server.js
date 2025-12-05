5d509fef88d924af34f042961f00412d5524f1b6
andriy <kozitskiyandriy@gmail.com>
2025-12-05 09:48:27 +0200

    add

---
+++ lab4/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+function getCurrentDateTime() {
+  const now = new Date();
+  const date = now.toLocaleDateString('uk-UA');
+  const time = now.toLocaleTimeString('uk-UA');
+  return { date, time };
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end('<h1>Головна сторінка</h1><p>Перейдіть на <a href="/time">/time</a> або <a href="/time/json">/time/json</a></p>');
+  }
+
+  else if (req.url === '/time') {
+    const now = getCurrentDateTime();
+    const message = `Поточний час: ${now.date}, ${now.time}`;
+
+    fs.appendFileSync('log.txt', `${message}\n`);
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(`<h1>${message}</h1>`);
+  }
+
+  else if (req.url === '/time/json') {
+    const now = getCurrentDateTime();
+
+    fs.appendFileSync('log.txt', `JSON запит: ${now.date}, ${now.time}\n`);
+
+    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    res.end(JSON.stringify(now));
+  }
+
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end('<h1>404 - Сторінку не знайдено</h1>');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Сервер працює на http://localhost:3000');
+});
